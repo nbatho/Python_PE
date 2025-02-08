0a6e1b64970aba6a7dd281d2e53f91ddbb7318f8
Pe <127192174+nbatho@users.noreply.github.com>
2025-02-08 11:08:50 +0700

    PYKT081 - Dia chi IP.py

---
+++ PYKT081 - Dia chi IP.py
@@ -0,0 +1,28 @@
+for _ in range(int(input())):
+    s = input()
+
+    i = 0
+    num = ""
+    ok = True
+    cnt = 0
+    while i < len(s):
+        if (s[i] >="0" and s[i] <= "9"):
+            num += s[i]
+        elif s[i] == ".":
+            if num == "" or int(num) > 255 or (num[0] == "0" and len(num) > 1):  
+                ok = False 
+                break
+            num = ""  
+            cnt += 1 
+        else:
+            ok = False
+            break
+        i+=1
+    if num == "" or int(num) > 255 or (num[0] == "0" and len(num) > 1):
+        ok = False
+    cnt += 1  
+
+    if ok and cnt == 4:
+        print("YES")
+    else:
+        print("NO")
