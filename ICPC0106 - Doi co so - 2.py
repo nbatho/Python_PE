6fddc2d9a79c19669bcac4c7492ac4f0321daa1d
Pe <127192174+nbatho@users.noreply.github.com>
2025-02-09 20:42:02 +0700

    ICPC0106 - Doi co so - 2.py

---
+++ ICPC0106 - Doi co so - 2.py
@@ -0,0 +1,38 @@
+def Convert(x,b,bac):
+    fullbinary =""
+    if len(x) % bac != 0:
+        for i in range(0,bac - len(x) % bac):
+            fullbinary += "0"
+    x = fullbinary + x
+    i = len(x) - 1
+    cnt = 0
+    bin = ""
+    ans = ""
+    while i != -1:
+        bin += x[i]
+        cnt += 1
+        num = 0
+        if cnt == bac:
+            for j in range(0,len(bin)):
+                num += (2**j)*int(bin[j])
+            if num < 10:
+                ans += str(num)
+            else:
+
+                ans += chr(ord("A") + (num -10))
+            num =0
+            cnt=0
+            bin = ""
+        i-=1
+    print(ans[::-1])
+for _ in range(int(input())):
+    b = int(input())
+    x = input()
+    bac = 0
+    if b == 2: print(x)
+    else:
+        if b == 4: bac = 2
+        elif b == 8: bac = 3
+        elif b == 16: bac = 4
+        Convert(x,b,bac)
+    
