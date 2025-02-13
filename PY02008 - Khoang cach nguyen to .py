9fcaa6632028c0551bc231b1d4bb99d100545222
Pe <127192174+nbatho@users.noreply.github.com>
2025-02-13 13:33:19 +0700

    PY02008 - Khoang cach nguyen to .py

---
+++ PY02008 - Khoang cach nguyen to .py
@@ -0,0 +1,24 @@
+import math
+limit =10001
+
+def seive(limit):
+    prime=[True]*limit
+    prime[0] = prime[1] =False
+    for i in range(2,int(math.sqrt(limit)) +1):
+        if prime[i]:
+            for j in range(i*i,limit,i):
+                prime[j] = False
+    primes =[]
+    for i in range(2,limit):
+        if prime[i]:
+            primes.append(i)
+    return primes
+                
+     
+n ,k= map(int,input().split())
+primes=seive(limit)
+cnt =0
+print(k,end=" ")
+for i in range(n):
+    k+=primes[i]
+    print(k,end=" ")
