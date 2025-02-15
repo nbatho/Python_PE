10da2afc7724fa5e6bc9b8c56d7d8817bf4dbf0d
Pe <127192174+nbatho@users.noreply.github.com>
2025-02-15 10:21:44 +0700

    PY02033 - Liet ke cac so co hai chu so theo thu tu xuat hien.py

---
+++ PY02033 - Liet ke cac so co hai chu so theo thu tu xuat hien.py
@@ -0,0 +1,15 @@
+s = input()
+cnt = 0
+num = ""
+ans = []
+for i in range(len(s)):
+    num += s[i]
+    cnt += 1
+    if cnt == 2:
+        if int(num) not in ans:
+            ans.append(int(num))
+        cnt = 0
+        num = ""
+
+for i in ans:
+    print(i,end = " ")
