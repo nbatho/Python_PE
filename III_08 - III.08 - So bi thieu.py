ed9a1031b9389c17afdc05d1ca8b276478a3ed2a
Pe <127192174+nbatho@users.noreply.github.com>
2025-03-07 14:52:30 +0700

    III_08 - III.08 - So bi thieu.py

---
+++ III_08 - III.08 - So bi thieu.py
@@ -0,0 +1,11 @@
+n = int(input())
+ls = []
+for _ in range(n-1):
+    ls.append(int(input()))
+cnt = 1
+ls.sort()
+for i in ls:
+    if i != cnt:
+        print(cnt)
+        break
+    else: cnt += 1
