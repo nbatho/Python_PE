60224f637e9ab432f26efaad4cdd5e066572adb3
Pe <127192174+nbatho@users.noreply.github.com>
2025-05-11 20:28:19 +0700

    PY02049 - Uoc so cua giai thua.py

---
+++ PY02049 - Uoc so cua giai thua.py
@@ -0,0 +1,16 @@
+"""
+    Legendre's formula 
+    sigma = n / p + n / p**2 + .. + n/ p**n
+"""
+def max_power_in_factorial(n, p):
+    x = 0
+    power = p
+    while power <= n:
+        x += n // power
+        power *= p
+    return x
+
+# Ví dụ:
+for _ in range(int(input())):
+    n,p = [int(x) for x in input().split()]
+    print(max_power_in_factorial(n, p)) 
